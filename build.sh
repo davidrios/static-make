5cd83b06cdd0667df5b3ea284c66fdbe99bfa12d
David Rios <david.rios.gomes@gmail.com>
2025-06-16 20:45:45 -0300

    Fix key

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ ARCHIVE="make-$VERSION.tar.gz"
 ZIG_VERSION="0.14.1"
 ARCH=$(uname -m)
 
+gpg --keyserver keyserver.ubuntu.com --recv-keys B2508A90102F8AE3B12A0090DEACCAAEDB78137A
+
 curl -LO "https://ziglang.org/download/$ZIG_VERSION/zig-$ARCH-linux-$ZIG_VERSION.tar.xz"
 tar xf "zig-$ARCH-linux-$ZIG_VERSION.tar.xz"
 export PATH="$PWD/zig-$ARCH-linux-$ZIG_VERSION:$PATH"
@@ -13,7 +15,6 @@ export PATH="$PWD/zig-$ARCH-linux-$ZIG_VERSION:$PATH"
 curl -LO "https://ftpmirror.gnu.org/make/$ARCHIVE"
 curl -LO "https://ftp.gnu.org/gnu/make/$ARCHIVE.sig"
 
-gpg --recv-keys B2508A90102F8AE3B12A0090DEACCAAEDB78137A
 gpg --verify "$ARCHIVE.sig" "$ARCHIVE"
 
 tar xf "$ARCHIVE"
