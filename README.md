8a689c63899cf1bbb113687874a0ab5a0bb68e0a
David Rios <david.rios.gomes@gmail.com>
2025-06-16 20:40:11 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # static-make
+
+Static builds of [GNU Make](https://www.gnu.org/software/make/) for Linux.
---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+VERSION="${VERSION:-4.4.1}"
+ARCHIVE="make-$VERSION.tar.gz"
+ZIG_VERSION="0.14.1"
+ARCH=$(uname -m)
+
+curl -LO "https://ziglang.org/download/$ZIG_VERSION/zig-$ARCH-linux-$ZIG_VERSION.tar.xz"
+tar xf "zig-$ARCH-linux-$ZIG_VERSION.tar.xz"
+export PATH="$PWD/zig-$ARCH-linux-$ZIG_VERSION:$PATH"
+
+curl -LO "https://ftpmirror.gnu.org/make/$ARCHIVE"
+curl -LO "https://ftp.gnu.org/gnu/make/$ARCHIVE.sig"
+
+gpg --recv-keys B2508A90102F8AE3B12A0090DEACCAAEDB78137A
+gpg --verify "$ARCHIVE.sig" "$ARCHIVE"
+
+tar xf "$ARCHIVE"
+
+cd "make-$VERSION"
+./configure CC="$CC"
+make
+mv make ..
